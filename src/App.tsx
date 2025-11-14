1689bd87a0869b1d49958f968a1f4ad11e30123a
Aidan Feldman <aidan.feldman@gmail.com>
2025-11-14 12:23:59 -0500

    use hash-based routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import SenatorsTable from "./components/SenatorsTable";
 import NomineesTable from "./components/NomineesTable";
 import SlatesTable from "./components/SlatesTable";
