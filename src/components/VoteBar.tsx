2f13a05d0cbc31a40819338a5b7a37b6b4886f7f
Aidan Feldman <aidan.feldman@gmail.com>
2025-11-22 02:34:28 -0500

    simplify VoteBar color specification

--- src/components/VoteBar.tsx
+++ src/components/VoteBar.tsx
@@ -25,28 +25,28 @@ const VoteBar: React.FC<VoteBarProps> = ({ ayes, nays, abs = 0, exc = 0 }) => {
       <div className="vote-bar">
         {ayes > 0 && (
           <div
-            className="vote-segment vote-segment-aye"
+            className="vote-segment vote-aye"
             style={{ width: `${ayePercent}%` }}
             title={`${ayes} Ayes (${ayePercent.toFixed(1)}%)`}
           />
         )}
         {nays > 0 && (
           <div
-            className="vote-segment vote-segment-nay"
+            className="vote-segment vote-nay"
             style={{ width: `${nayPercent}%` }}
             title={`${nays} Nays (${nayPercent.toFixed(1)}%)`}
           />
         )}
         {abs > 0 && (
           <div
-            className="vote-segment vote-segment-abs"
+            className="vote-segment vote-abs"
             style={{ width: `${absPercent}%` }}
             title={`${abs} Absent (${absPercent.toFixed(1)}%)`}
           />
         )}
         {exc > 0 && (
           <div
-            className="vote-segment vote-segment-exc"
+            className="vote-segment vote-exc"
             style={{ width: `${excPercent}%` }}
             title={`${exc} Excused (${excPercent.toFixed(1)}%)`}
           />
@@ -55,25 +55,25 @@ const VoteBar: React.FC<VoteBarProps> = ({ ayes, nays, abs = 0, exc = 0 }) => {
       <div className="vote-legend">
         {ayes > 0 && (
           <span className="vote-legend-item">
-            <span className="vote-legend-color vote-legend-aye"></span>
+            <span className="vote-legend-color vote-aye"></span>
             Aye: {ayes}
           </span>
         )}
         {nays > 0 && (
           <span className="vote-legend-item">
-            <span className="vote-legend-color vote-legend-nay"></span>
+            <span className="vote-legend-color vote-nay"></span>
             Nay: {nays}
           </span>
         )}
         {abs > 0 && (
           <span className="vote-legend-item">
-            <span className="vote-legend-color vote-legend-abs"></span>
+            <span className="vote-legend-color vote-abs"></span>
             Abs: {abs}
           </span>
         )}
         {exc > 0 && (
           <span className="vote-legend-item">
-            <span className="vote-legend-color vote-legend-exc"></span>
+            <span className="vote-legend-color vote-exc"></span>
             Exc: {exc}
           </span>
         )}
